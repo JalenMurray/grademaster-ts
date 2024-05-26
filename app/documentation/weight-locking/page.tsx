28880cea65d928e292c0eba48934fa836b31570d
Jalen Murray <jalenmmurray@gmail.com>
2024-05-26 17:54:14 -0400

    Eslint

--- app/documentation/weight-locking/page.tsx
+++ app/documentation/weight-locking/page.tsx
@@ -31,10 +31,10 @@ export default function Page() {
           each assignment type. For example, we are 2 weeks in and it seems like we homework every
           week and it was a 15 week course, I would create the formula 5/15, and copy it to each
           cell for the weight of an assignment. This in itself is not the worst thing to have to do
-          and is pretty manageable. But what if we get to week 3 and we don't have any homework. Now
-          all my formulas are messed up and I have to change all of them, or change one and copy it
-          over to the rest. This is something that would happen to me a lot. So when I was creating
-          GradeMaster, this was one of the things I was sure to address.
+          and is pretty manageable. But what if we get to week 3 and we don&apos;t have any
+          homework. Now all my formulas are messed up and I have to change all of them, or change
+          one and copy it over to the rest. This is something that would happen to me a lot. So when
+          I was creating GradeMaster, this was one of the things I was sure to address.
         </p>
         <p>
           So when would you use weight locking? When you are given a grade breakdown where weights
@@ -56,11 +56,11 @@ export default function Page() {
           type for homework, lock the weights, and assign it the weight 5. Now whenever I create or
           delete assignments, the weights for all the assignments of that type will be balanced. So
           in our example, I assumed I will have 15 homeworks and created 15 homework assignments in
-          my homeworks assignment type. I didn't have to do any calculations and GradeMaster set
-          each weight to 5 / 15 = .33. But once I got to week 3 and there was no homework, if I
-          assume that the pattern still holds up but I we just didn't have one this week, I simply
-          have to delete one of the assignments. Now all 14 of the other assignments are updated to
-          5 / 14 = .36 instantly!
+          my homeworks assignment type. I didn&apos;t have to do any calculations and GradeMaster
+          set each weight to 5 / 15 = .33. But once I got to week 3 and there was no homework, if I
+          assume that the pattern still holds up but I we just didn&apos;t have one this week, I
+          simply have to delete one of the assignments. Now all 14 of the other assignments are
+          updated to 5 / 14 = .36 instantly!
         </p>
       </section>
     </div>
